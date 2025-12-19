d949e72f568e8214ee3937bb89046cec8f44700f
Randa458 <randahamada270@gmail.com>
2025-12-19 22:32:44 +0200

    for test

---
+++ src/python-for-ml/Session3/std.py
@@ -0,0 +1,15 @@
+def std_inf0(name: str , age: int):
+    """
+    this function take student information like name and age 
+    
+    param_1 : take user name from user
+    type param_1 : str
+    
+    param_2 : take age from user
+    type param_2 : int
+    
+    return : print student information for the name and age 
+    type return : str , int
+    
+    """
+    return f"my name is {name }, and my age is {age}."
