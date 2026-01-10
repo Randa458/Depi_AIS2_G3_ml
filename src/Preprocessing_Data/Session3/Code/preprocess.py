e84906e327d9307f6939ad02922a882203e337d6
Randa458 <randahamada270@gmail.com>
2026-01-10 08:44:05 +0200

    for test

---
+++ src/Preprocessing_Data/Session3/Code/preprocess.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def chk_types(data):
+    dtypes = data.dtypes
+    n_uniques = data.nunique()
+    return pd.DataFrame({"Dtype":dtypes, "Num_Uniques":n_uniques}).T
+
+
+def chk_null(data):
+    null = data.isnull().sum()
+    ratio = (null / data.shape[0]) * 100
+    return pd.DataFrame({"Null Count": null,"Null %": ratio.round(2)}).T
